d7fe03ab022b4c074d84650cbda676e34d9370a0
mene13 <mene13@Xubuntu.myguest.virtualbox.org>
2024-05-25 18:15:14 +0300

    feat: added babel

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,18 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: [
+                      ['@babel/preset-env', { targets: "defaults" }]
+                    ]
+                  }
+                }
+            },
+            {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
             },
